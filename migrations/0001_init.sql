d95c104182e795a264d780825f86a87031bfde1f
gogazub <gogazub101@gmail.com>
2025-11-13 21:49:31 +0300

    feat: add finalize the init migration

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -1,11 +1,12 @@
+--- Основные таблицы ---
 CREATE TABLE users (
     user_id UUID PRIMARY KEY, 
     name TEXT NOT NULL,
-    is_active BOOLEAN NOT NULL DEFAULT FALSE
+    is_active BOOLEAN NOT NULL DEFAULT FALSE,
 
     -- Добавим метки со временем на будущее
-    created_at TIMESTAMPTZ DEFAULT NOW()
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
     deleted_at TIMESTAMPTZ NULL
 );
 
@@ -15,25 +16,17 @@ CREATE TABLE users (
 -- Так что для гибкости добавим id 
 CREATE TABLE teams (
     team_id UUID PRIMARY KEY,
-    team_name TEXT
+    team_name TEXT UNIQUE,
 
-    created_at TIMESTAMPTZ DEFAULT NOW()
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
     deleted_at TIMESTAMPTZ NULL
-);
-
--- Допустим, что юзер может находится сразу в двух командах. Тогда имеем one to many.
-CREATE TABLE user_teams (
-    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
-    team_id UUID REFERENCES teams(team_id) ON DELETE CASCADE,
-    PRIMARY KEY (user_id, team_id)
 );
 
 CREATE TYPE pull_request_status AS ENUM (
     'OPEN',
     'MERGED'
 );
-
 CREATE TABLE pull_requests (
     pr_id UUID PRIMARY KEY,
     name TEXT,
@@ -44,9 +37,19 @@ CREATE TABLE pull_requests (
     --first_reviewers_id UUID REFERENCES users(user_id) DEFAULT NULL,
     --second_reviewers_id UUID REFERENCES users(user_id) DEFAULT NULL
 
-    created_at TIMESTAMPTZ DEFAULT NOW()
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
     deleted_at TIMESTAMPTZ NULL
+);
+
+
+--- Отношения между основными таблицами ---
+
+-- Допустим, что юзер может находится сразу в двух командах. Тогда имеем one to many.
+CREATE TABLE user_teams (
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    team_id UUID REFERENCES teams(team_id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, team_id)
 );
 
 -- Отдельная таблица для связи pr и reviewrs. Это таблицу стоит вынести, потому в дальнейшем можно будет легко увеличить кол-во ревьюеров  
@@ -55,4 +58,35 @@ CREATE TABLE pr_reviewers (
     user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
     assigned_at TIMESTAMPTZ DEFAULT NOW(),
     PRIMARY KEY (pr_id, user_id)
+    -- assigned_at
 );
+
+
+--- Функции ---
+
+-- Функция для обновления updated_at
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Триггер обновления updated_at при апдейте в users
+CREATE TRIGGER users_updated_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+-- Триггер обновления updated_at при апдейте в teams
+CREATE TRIGGER teams_updated_timestamp
+BEFORE UPDATE ON teams
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
+
+-- Триггер обновления updated_at при апдейте в pull_requests
+CREATE TRIGGER pull_requests_updated_timestamp
+BEFORE UPDATE ON pull_requests
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamp();
