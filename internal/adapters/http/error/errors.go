0c4ffd4aade350c84870a6b2101316194adb06d2
gogazub <gogazub101@gmail.com>
2025-11-21 22:45:48 +0300

    feat: add small error processing

--- internal/adapters/http/error/errors.go
+++ internal/adapters/http/error/errors.go
@@ -2,6 +2,7 @@ package httperror
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -27,27 +28,32 @@ type ErrorResponseDTO struct {
 	Error ErrorDTO `json:"error"`
 }
 
-
 // Так как в спецификации нет подходящей ошибки invalid json,
 // то вынуждено проставим ErrorCodeNotFound и пояснения в message
 func WriteBadRequest(w http.ResponseWriter, msg string) {
 	w.WriteHeader(http.StatusBadRequest)
-	// TODO: process error
-	json.NewEncoder(w).Encode(ErrorResponseDTO{
+	err := json.NewEncoder(w).Encode(ErrorResponseDTO{
 		Error: ErrorDTO{
 			Code:    ErrorCodeNotFound,
 			Message: msg,
 		},
 	})
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		fmt.Printf("json encoding error: %v", err)
+	}
 }
 
 func WriteErrorResponse(w http.ResponseWriter, status int, code ErrorCode, msg string) {
 	w.WriteHeader(status)
-	// process error
-	json.NewEncoder(w).Encode(ErrorResponseDTO{
+	err := json.NewEncoder(w).Encode(ErrorResponseDTO{
 		Error: ErrorDTO{
 			Code:    code,
 			Message: msg,
 		},
 	})
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		fmt.Printf("json encoding error: %v", err)
+	}
 }
